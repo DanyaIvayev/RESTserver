5717fb1f2f5ec4f5804192162c68ea05f01af7a1
DanyaIvayev <DanyaIvayev@yandex.ru>
2015-11-06 14:55:06 +0400

    Fix running jetty server

--- src/example/HelloWorld.java
+++ src/example/HelloWorld.java
@@ -1,29 +1,46 @@
 package example;
-import com.sun.net.httpserver.HttpServer;
-import com.sun.jersey.api.container.httpserver.HttpServerFactory;
+/*import com.sun.net.httpserver.HttpServer;
+import com.sun.jersey.api.container.httpserver.HttpServerFactory;*/
 import java.io.IOException;
 
-import javax.ws.rs.GET;
+/*import javax.ws.rs.GET;
 import javax.ws.rs.Produces;
-import javax.ws.rs.Path;
+import javax.ws.rs.Path;*/
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 /**
- * Created by 0<8@ on 05.11.2015.
+ * Created by Danis on 05.11.2015.
  */
 // The Java class will be hosted at the URI path "/helloworld"
-@Path("/helloworld")
+
 public class HelloWorld {
     // The Java method will process HTTP GET requests
-    @GET
+   /* @GET
     // The Java method will produce content identified by the MIME Media type "text/plain"
     @Produces("text/plain")
     public String getClichedMessage() {
         // Return some cliched textual content
         return "Hello World";
-    }
+    }*/
+    @SuppressWarnings("serial")
+    public static class HelloServlet extends HttpServlet {
 
+        @Override
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+            response.setContentType("text/html");
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().println("<h1>Hello SimpleServlet</h1>");
+        }
+
+    }
     public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServerFactory.create("http://localhost:9998/");
+       /* HttpServer server = HttpServerFactory.create("http://localhost:9998/");
         server.start();
 
         System.out.println("Server running");
@@ -33,5 +50,23 @@ public class HelloWorld {
         System.out.println("Stopping server");
         server.stop(0);
         System.out.println("Server stopped");
+*/      Server server =null;
+        try {
+            server = new Server(8080);
+            ServletHandler handler = new ServletHandler();
+            //handler.addServlet(new ServletHolder(new HelloServlet()));
+            ServletHolder holder = handler.addServletWithMapping(HelloServlet.class, "/hello");//Set the servlet to run.
+            server.setHandler(handler);
+            server.start();
+            server.join();
+        } catch(java.lang.Exception e){
+            try {
+                if (server != null)
+                    server.stop();
+            } catch (Exception e1){
+                e.printStackTrace();
+            }
+        }
     }
+
 }
---
+++ web/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Дамир
+  Date: 06.11.2015
+  Time: 12:52
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>REST SERVER MEETING APP</title>
+</head>
+<body>
+
+</body>
+</html>
